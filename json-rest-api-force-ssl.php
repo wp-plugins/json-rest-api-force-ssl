7032bf9ab817169bfd2eaf964949407aed6c3086
fjarrett <fjarrett@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-31 06:42:56 +0000

    Update to commit 6d31da7 from git@github.com:fjarrett/json-rest-api-force-ssl.git
    git-svn-id: https://plugins.svn.wordpress.org/json-rest-api-force-ssl/tags/0.1.1@1057326 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ json-rest-api-force-ssl.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * Plugin Name: JSON REST API Force SSL
+ * Description: Force WP JSON REST API endpoints to always be served over HTTPS.
+ * Version: 0.1.1
+ * Depends: JSON REST API
+ * Author: Frankie Jarrett
+ * Author URI: http://frankiejarrett.com
+ * License: GPLv2+
+ * Text Domain: json-rest-api-force-ssl
+ */
+
+class JSON_REST_API_Force_SSL {
+
+	/**
+	 * Plugin version number
+	 *
+	 * @const string
+	 */
+	const VERSION = '0.1.1';
+
+	/**
+	 * Hold plugin instance
+	 *
+	 * @var string
+	 */
+	public static $instance;
+
+	/**
+	 * Class constructor
+	 */
+	private function __construct() {
+		add_action( 'plugins_loaded', array( __CLASS__, 'load' ) );
+		add_action( 'wp_json_server_before_serve', array( __CLASS__, 'ssl_redirect' ) );
+	}
+
+	/**
+	 * Check the dependencies for this plugin
+	 *
+	 * @action plugins_loaded
+	 *
+	 * @return void
+	 */
+	public static function load() {
+		if ( defined( 'JSON_API_VERSION' ) ) {
+			return;
+		}
+
+		// Display admin notice
+		add_action( 'all_admin_notices', array( __CLASS__, 'admin_notice' ) );
+
+		// Deactivate the plugin
+		add_action( 'admin_init', array( __CLASS__, 'deactivate' ) );
+	}
+
+	/**
+	 * Display admin notice when JSON REST API does not exist
+	 *
+	 * @action all_admin_notices
+	 *
+	 * @return void
+	 */
+	public static function admin_notice() {
+		?>
+		<div class="error">
+			<p><?php _e( 'The <strong>JSON REST API Force SSL</strong> plugin has been deactivated because it requires the <strong>JSON REST API</strong> plugin.', 'json-rest-api-force-ssl' ) ?></p>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Deactivate the plugin
+	 *
+	 * @action admin_init
+	 *
+	 * @return void
+	 */
+	public static function deactivate() {
+		deactivate_plugins( plugin_basename( __FILE__ ) );
+	}
+
+	/**
+	 * Force WP JSON REST API endpoints to always be served over HTTPS
+	 *
+	 * @action wp_json_server_before_serve
+	 *
+	 * @return void
+	 */
+	public static function ssl_redirect() {
+		if ( is_ssl() ) {
+			return;
+		}
+
+		$json_url = esc_url_raw( $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] );
+		$redirect = set_url_scheme( $json_url, 'https' );
+
+		wp_safe_redirect( $redirect, 301 );
+
+		exit;
+	}
+
+	/**
+	 * Return active instance of JSON_REST_API_Force_SSL, create one if it doesn't exist
+	 *
+	 * @return JSON_REST_API_Force_SSL
+	 */
+	public static function get_instance() {
+		if ( empty( self::$instance ) ) {
+			$class = __CLASS__;
+			self::$instance = new $class;
+		}
+
+		return self::$instance;
+	}
+
+}
+
+$GLOBALS['json_rest_api_force_ssl'] = JSON_REST_API_Force_SSL::get_instance();
---
+++ readme.txt
@@ -0,0 +1,37 @@
+=== JSON REST API Force SSL ===
+Contributors:      fjarrett
+Tags:              api, json, REST, rest-api, ssl, https, security
+Requires at least: 3.9
+Tested up to:      4.1
+Stable tag:        trunk
+License:           GPLv2 or later
+License URI:       http://www.gnu.org/licenses/gpl-2.0.html
+
+Force WP JSON REST API endpoints to always be served over HTTPS.
+
+== Description ==
+
+**Note: This plugin requires the [JSON REST API (WP API)](https://wordpress.org/plugins/json-rest-api/) plugin.**
+
+For site owners who want the WP API served over SSL always and forever. All HTTP requests to the JSON REST API will be 301 redirected to their HTTPS equivalent.
+
+`http://example.com/wp-json/ --> https://example.com/wp-json/`  
+`http://example.com/wp-json/posts/ --> https://example.com/wp-json/posts/`
+
+If for some reason you don't want to use a plugin to do this, you can also just add this hook to your theme: https://gist.github.com/fjarrett/8cd04bd33e49bbd6e3b7
+
+And as always, if you have suggestions to make this plugin better, or find problems with it, please create an issue on [GitHub](https://github.com/fjarrett/json-rest-api-force-ssl).
+
+== Changelog ==
+
+= 0.1.1 - December 30, 2014 =
+
+* Ensure safe local redirects by using wp_safe_redirect() instead of wp_redirect()
+
+Props [johnjamesjacoby](https://profiles.wordpress.org/johnjamesjacoby/)
+
+= 0.1.0 - December 30, 2014 =
+
+* Initial release.
+
+Props [fjarrett](https://profiles.wordpress.org/fjarrett/)
